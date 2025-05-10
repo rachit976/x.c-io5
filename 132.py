21946fb6918d721d4645dda95e22f8014f36e973
rachit976 <mukeshsoni38@gmail.com>
2025-05-10 19:49:42 +0530

    Add files via upload

---
+++ 132.py
@@ -0,0 +1,15 @@
+class iostring():
+
+
+    def_init_(self):
+    self.str1 =""
+    def get_string(self):self.str1 = input('enter string:')
+
+
+    def print_string(self):
+        print('resultis:',self.str1.upper())
+
+
+        str1 =iostring()
+        str1.get_string
+
